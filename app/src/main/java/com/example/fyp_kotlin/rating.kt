1cf39fc96592ab75f86aa0c26f162f631136c06a
Alvin <alvinchakweb@gamil.com>
2023-06-04 20:46:19 +0800

    UI desing for result page

--- app/src/main/java/com/example/fyp_kotlin/rating.kt
+++ app/src/main/java/com/example/fyp_kotlin/rating.kt
@@ -19,7 +19,23 @@ class Rating : AppCompatActivity() {
     private lateinit var postSugars: String
     private lateinit var postSodium: String
 
+    fun getGradeDrawable(grade: String): Int {
+        return when (grade) {
+            "A++" -> R.drawable.grade_a_plus_plus
+            "A+" -> R.drawable.grade_a_plus
+            "A" -> R.drawable.grade_a
+            "B" -> R.drawable.grade_b
+            "C" -> R.drawable.grade_c
+            "D" -> R.drawable.grade_d
+            "E" -> R.drawable.grade_e
+            "E-" -> R.drawable.grade_e_minus
+            "E--" -> R.drawable.grade_e_minus_minus
+            else -> R.drawable.grade_e_minus_minus
+        }
+    }
+
     fun fetchNutritions() {
+        setLoadingIndicator(true)
         var foodType : String = postFoodType.toString()
         var totalFat : String = postTotalfat.toString()
         var sugars : String = postSugars.toString()
@@ -46,7 +62,27 @@ class Rating : AppCompatActivity() {
         })
     }
 
+    fun setLoadingIndicator(isLoading: Boolean) {
+        val progressBar = findViewById<ProgressBar>(R.id.loading_progress_bar)
+        val gradeImageView = findViewById<ImageView>(R.id.grade_image_view)
+        val scoreTextView = findViewById<TextView>(R.id.score_text_view)
+        val saveFavouriteLayout = findViewById<LinearLayout>(R.id.save_to_favourite)
+
+        if (isLoading) {
+            progressBar.visibility = View.VISIBLE
+            gradeImageView.visibility = View.GONE
+            scoreTextView.visibility = View.GONE
+            saveFavouriteLayout.visibility = View.GONE
+        } else {
+            progressBar.visibility = View.GONE
+            gradeImageView.visibility = View.VISIBLE
+            scoreTextView.visibility = View.VISIBLE
+            saveFavouriteLayout.visibility = View.VISIBLE
+        }
+    }
+
     fun processJSON(jsonString : String) {
+        setLoadingIndicator(false)
         nutritions.clear()
         val jsonArray = JSONArray(jsonString)
         for (i in 0 until jsonArray.length()) {
@@ -57,10 +93,14 @@ class Rating : AppCompatActivity() {
             val nutrition = Nutrition(id, score, grade)
             nutritions.add(nutrition)
         }
-        val adapter = NutritionAdapter(this, android.R.layout.simple_expandable_list_item_2,
-            nutritions)
-        val listView = findViewById<ListView>(R.id.listView)
-        listView.adapter = adapter
+        val nutrition = nutritions[0]
+        // 更新 grade ImageView
+        val gradeImageView = findViewById<ImageView>(R.id.grade_image_view)
+        gradeImageView.setImageResource(getGradeDrawable(nutrition.grade))
+
+        // 更新 score TextView
+        val scoreTextView = findViewById<TextView>(R.id.score_text_view)
+        scoreTextView.text = "Score: ".plus(nutrition.score)
     }
 
     override fun onResume() {
